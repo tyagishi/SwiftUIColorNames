3c7f9fd9c683309da572762111e573033d45026b
Tomoaki Yagishita <tyagishi@gmail.com>
2020-10-24 16:29:03 +0900

    initial version works well

--- Tests/SwiftUIColorNamesTests/SwiftUIColorNamesTests.swift
+++ Tests/SwiftUIColorNamesTests/SwiftUIColorNamesTests.swift
@@ -6,7 +6,6 @@ final class SwiftUIColorNamesTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftUIColorNames().text, "Hello, World!")
     }
 
     static var allTests = [
